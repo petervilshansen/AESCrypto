20c071fd2f43be98d295a3d7efc3b1d334f2fe98
Peter Vils Hansen <peter@vilshansen.dk>
2022-04-28 20:06:22 +0200

    Added EncryptedBlock

---
+++ AESCrypto2021/EncryptedBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AESCrypto2021
+{
+  public class EncryptedBlock
+  {
+    public byte[] tag { get; set; }
+    public byte[] cipherText { get; set; }
+    public byte[] keySalt { get; set; }
+    public byte[] nonce { get; set; }
+  }
+}
