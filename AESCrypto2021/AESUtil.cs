fc2e236938da6c4d4e04272beb6b478fcea96cd9
Peter Vils Hansen <peter.vils.hansen@outlook.com>
2022-07-13 07:35:59 +0200

    Many updates.

--- AESCrypto2021/AESUtil.cs
+++ AESCrypto2021/AESUtil.cs
@@ -1,98 +1,93 @@
-﻿using System;
-using System.Buffers.Binary;
-using System.IO;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace AESCrypto2021
 {
-  public static class AESUtil
-  {
-    public const int SALT_SIZE = 8; // size in bytes
-    public const int KEY_SIZE = 32; // size in bytes
-    public const int PBKDF2_ITERATIONS = 250000;
-
-    internal static byte[] deriveEnryptionKey(string clearTextPassword, Span<byte> salt)
+    public static class AESUtil
     {
-      Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(Encoding.UTF8.GetBytes(clearTextPassword), salt.ToArray(), PBKDF2_ITERATIONS);
-      byte[] encryptionKey = pbkdf2.GetBytes(KEY_SIZE);
-      return encryptionKey;
+        public const int SALT_SIZE = 8; // size in bytes
+        public const int NONCE_SIZE = 12; // size in bytes
+        public const int KEY_SIZE = 32; // size in bytes = 256 bits
+        public const int PBKDF2_ITERATIONS = 200001;
+        public const int AUTH_TAG_SIZE = 16;
+        private const int PASSWORD_LENGTH = 25; // length of automatically generated secure password
+        private const string TOKEN_DELIMITER = ".";
+
+        internal static string CreatePassword(int length)
+        {
+            if (length % 5 != 0)
+            {
+                throw new ArgumentException("Password length must be a multiple of 5 in order to facilitate pretty-printing.");
+            }
+            const string valid = "abcdefghijkmnopqrstuvwxyz";
+            StringBuilder res = new StringBuilder();
+            while (0 < length--)
+            {
+                res.Append(valid[RandomNumberGenerator.GetInt32(valid.Length)]);
+                if (length % 5 == 0 && length > 0)
+                {
+                    res.Append("-"); // pretty-print password for user
+                }
+            }
+            return res.ToString();
+        }
+        internal static byte[] deriveEnryptionKey(string clearTextPassword, Span<byte> salt)
+        {
+            // Rfc2898DeriveBytes has been replaced in .NET 6.0:
+            // https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-6.0
+            byte[] encryptionKey = KeyDerivation.Pbkdf2(clearTextPassword, salt.ToArray(), KeyDerivationPrf.HMACSHA256, PBKDF2_ITERATIONS, KEY_SIZE);
+            return encryptionKey;
+        }
+
+        public static string Encrypt(byte[] plainBytes)
+        {
+            if (plainBytes == null || plainBytes.Length == 0) { throw new Exception("Plaintext cannot be null or empty."); }
+
+            // Generate secure nonce and salt
+            var salt = RandomNumberGenerator.GetBytes(SALT_SIZE);
+            var nonce = RandomNumberGenerator.GetBytes(NONCE_SIZE);
+            var cipherBytes = new byte[plainBytes.AsSpan().Length];
+            var authTag = new byte[AUTH_TAG_SIZE];
+
+            // Encrypt
+            string randomPassword = CreatePassword(PASSWORD_LENGTH);
+            Console.WriteLine("Automatically generated secure password (write this down): " + randomPassword);
+         
+            var derivedEncryptionKey = deriveEnryptionKey(randomPassword, salt);
+            using (var aes = new AesGcm(derivedEncryptionKey))
+            {
+                aes.Encrypt(nonce, plainBytes.AsSpan(), cipherBytes, authTag);
+            }
+
+            // Encode to Base64 for easy transmission
+            return Convert.ToBase64String(salt) + TOKEN_DELIMITER + Convert.ToBase64String(nonce) + TOKEN_DELIMITER + Convert.ToBase64String(cipherBytes) + TOKEN_DELIMITER + Convert.ToBase64String(authTag);
+        }
+
+
+        public static byte[] Decrypt(string cipherText, string clearTextPassword)
+        {
+            // Decode
+            var tokens = cipherText.Split(TOKEN_DELIMITER);
+            if (tokens.Length != 4)
+            {
+                throw new Exception("Bad encrypted data -- expected encrypted data format is: <SALT>.<NONCE>.<CIPHER_TEXT>.<AUTH_TAG> (all Base64-encoded).");
+            }
+
+            // Extract parameters
+            var salt = Convert.FromBase64String(tokens[0]).AsSpan();
+            var nonce = Convert.FromBase64String(tokens[1]).AsSpan();
+            var cipherBytes = Convert.FromBase64String(tokens[2]).AsSpan();
+            var authTag = Convert.FromBase64String(tokens[3]).AsSpan();
+
+            // Decrypt
+            Span<byte> plainBytes = new byte[cipherBytes.Length];
+            byte[] derivedKey = deriveEnryptionKey(clearTextPassword, salt);
+            using var aes = new AesGcm(derivedKey);
+            aes.Decrypt(nonce, cipherBytes, authTag, plainBytes);
+
+            return plainBytes.ToArray();
+        }
     }
-    public static string Encrypt(string plainText, string clearTextPassword)
-    {
-      return Encrypt(Encoding.UTF8.GetBytes(plainText), clearTextPassword);
-    }
-    public static string Encrypt(byte[] plainBytes, string clearTextPassword)
-    {
-      if (plainBytes == null || plainBytes.Length == 0) { throw new Exception("Plaintext cannot be null or empty."); }
-
-      // Get parameter sizes
-      int saltSize = SALT_SIZE;
-      int nonceSize = AesGcm.NonceByteSizes.MaxSize;
-      int tagSize = AesGcm.TagByteSizes.MaxSize;
-      int cipherSize = plainBytes.Length;
-
-      // We write everything into one big array for easier encoding
-      int encryptedDataLength = 4 + saltSize + 4 + nonceSize + 4 + tagSize + cipherSize;
-      Span<byte> encryptedData = encryptedDataLength < 1024
-                               ? stackalloc byte[encryptedDataLength]
-                               : new byte[encryptedDataLength].AsSpan();
-
-      // Copy parameters
-      BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(0, 4), saltSize);
-      BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(4 + saltSize, 4), nonceSize);
-      BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(4 + saltSize + 4 + nonceSize, 4), tagSize);
-
-      var salt = encryptedData.Slice(4, saltSize);
-      var nonce = encryptedData.Slice(4 + saltSize + 4, nonceSize);
-      var tag = encryptedData.Slice(4 + saltSize + 4 + nonceSize + 4, tagSize);
-      var cipherBytes = encryptedData.Slice(4 + saltSize + 4 + nonceSize + 4 + tagSize, cipherSize);
-
-      // Generate secure nonce and salt
-      RandomNumberGenerator.Fill(salt);
-      RandomNumberGenerator.Fill(nonce);
-
-      // Encrypt
-      using var aes = new AesGcm(deriveEnryptionKey(clearTextPassword, salt));
-      aes.Encrypt(nonce, plainBytes.AsSpan(), cipherBytes, tag);
-
-      // Encode for transmission
-      return Convert.ToBase64String(encryptedData);
-    }
-
-    public static byte[] Decrypt(string cipherText, string clearTextPassword)
-    {
-      // Decode
-      Span<byte> encryptedData = Convert.FromBase64String(cipherText).AsSpan();
-
-      // Extract parameter sizes
-      int saltSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(0, 4));
-      int nonceSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(4 + saltSize, 4));
-      int tagSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(4 + saltSize + 4 + nonceSize, 4));
-      int cipherTextSize = encryptedData.Length - 4 - saltSize - 4 - nonceSize - 4 - tagSize;
-
-      // Extract parameters
-      var salt = encryptedData.Slice(4, saltSize);
-      var nonce = encryptedData.Slice(4 + saltSize + 4, nonceSize);
-      var tag = encryptedData.Slice(4 + saltSize + 4 + nonceSize + 4, tagSize);
-      var cipherBytes = encryptedData.Slice(4 + saltSize + 4 + nonceSize + 4 + tagSize, cipherTextSize);
-
-      // Decrypt
-      Span<byte> plainBytes = cipherTextSize < 1024
-                            ? stackalloc byte[cipherTextSize]
-                            : new byte[cipherTextSize];
-      using var aes = new AesGcm(deriveEnryptionKey(clearTextPassword, salt));
-      try
-      {
-        aes.Decrypt(nonce, cipherBytes, tag, plainBytes);
-      } catch (CryptographicException ce)
-      {
-        throw new Exception("Error during decryption: " + ce.Message);
-      }
-
-      return plainBytes.ToArray();
-    }
-  }
 }
-
--- AESCrypto2021/Program.cs
+++ AESCrypto2021/Program.cs
@@ -1,81 +1,111 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Text.Json;
 
 namespace AESCrypto2021
 {
     class Program
     {
-        private static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
-        {
-            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-            WriteIndented = true
-        };
-
         static void Main(string[] args)
         {
             CheckCommandLineArguments(args);
 
-            if (args[0].ToLower().Equals("-ec"))
+            if (Console.IsInputRedirected)
             {
-                string password = args[1];
-                EncryptConsoleInput(password);
+                Console.WriteLine("Input redirected is not supported. Please try again without redirecting input via pipes.");
+                return;
             }
-            else if (args[0].ToLower().Equals("-ef"))
+
+            string operation = args[0].ToLower();
+
+            try
             {
-                string password = args[1];
-                string inputFileName = args[2];
-                string outputFileName = args[3];
-                EncryptFileInput(inputFileName, outputFileName, password);
-            }
-            else if (args[0].ToLower().Equals("-dc"))
+                if ("-ec".Equals(operation))
+                {
+                    string plainText = readInputFromConsole();
+                    EncryptConsoleInput(plainText);
+                }
+                else if ("-ef".Equals(operation))
+                {
+                    if (args.Length < 3 || string.IsNullOrEmpty(args[1]) || string.IsNullOrEmpty(args[2]))
+                    {
+                        throw new ArgumentException("Input file name or output file name is missing -- aborting...");
+                    }
+                    string inputFileName = args[1];
+                    string outputFileName = args[2];
+                    EncryptFileInput(inputFileName, outputFileName);
+                }
+                else if ("-dc".Equals(operation))
+                {
+                    string cipherText = readInputFromConsole();
+                    DecryptConsoleInput(cipherText);
+                }
+                else if ("-df".Equals(operation))
+                {
+                    if (args.Length < 3 || string.IsNullOrEmpty(args[1]) || string.IsNullOrEmpty(args[2]))
+                    {
+                        throw new ArgumentException("Input file name or output file name is missing -- aborting...");
+                    }
+                    string password = readPassphraseFromConsole();
+                    string inputFileName = args[1];
+                    string outputFileName = args[2];
+                    DecryptFileInput(inputFileName, outputFileName, password);
+                }
+                else
+                {
+                    printUsage();
+                }
+            } catch (Exception ex)
             {
-                string password = args[1];
-                DecryptConsoleInput(password);
-            }
-            else if (args[0].ToLower().Equals("-df"))
-            {
-                string password = args[1];
-                string inputFileName = args[2];
-                string outputFileName = args[3];
-                DecryptFileInput(inputFileName, outputFileName, password);
-            }
-            else
-            {
-                printUsage();
+                Console.WriteLine("Operation failed with message: " + ex.Message);
             }
         }
 
         private static void DecryptFileInput(string inputFileName, string outputFileName, string password)
         {
             byte[] decrypted = AESUtil.Decrypt(File.ReadAllText(inputFileName), password);
+            if (decrypted == null)
+            {
+                return;
+            }
             File.WriteAllBytes(outputFileName, decrypted);
         }
 
-        private static void DecryptConsoleInput(string password)
+        private static void DecryptConsoleInput(string cipherText)
         {
-            string encrypted = readInputFromConsole();
-            string decrypted = Encoding.UTF8.GetString(AESUtil.Decrypt(encrypted, password));
+            string password = readPassphraseFromConsole();
+            string decrypted = Encoding.UTF8.GetString(AESUtil.Decrypt(cipherText, password));
+            if (string.IsNullOrEmpty(decrypted))
+            {
+                return;
+            }
             Console.WriteLine(decrypted);
         }
 
-        private static void EncryptFileInput(string inputFileName, string outputFileName, string password)
+        private static void EncryptFileInput(string inputFileName, string outputFileName)
         {
-            string encryptedBase64 = AESUtil.Encrypt(File.ReadAllBytes(inputFileName), password);
+            string encryptedBase64 = AESUtil.Encrypt(File.ReadAllBytes(inputFileName));
+            if (string.IsNullOrEmpty(encryptedBase64))
+            {
+                return;
+            }
             File.WriteAllText(outputFileName, encryptedBase64);
         }
 
-        private static void EncryptConsoleInput(string password)
+        private static void EncryptConsoleInput(string plainText)
         {
-            string encrypted = AESUtil.Encrypt(readInputFromConsole(), password);
+            string encrypted = AESUtil.Encrypt(Encoding.UTF8.GetBytes(plainText));
+            if (string.IsNullOrEmpty(encrypted))
+            {
+                return;
+            }
             Console.WriteLine(encrypted);
         }
 
         private static void CheckCommandLineArguments(string[] args)
         {
-            if (args == null || args.Length < 2 || args.Length > 4) printUsage();
-            if (args.Length == 2 && !(args[0].ToLower().Equals("-ec") || args[0].ToLower().Equals("-dc"))) printUsage();
+            if (args == null || args.Length != 1 && args.Length != 3) printUsage();
+            if (args.Length == 1 && !(args[0].ToLower().Equals("-ec") || args[0].ToLower().Equals("-dc"))) printUsage();
             if (args.Length == 3 && !(args[0].ToLower().Equals("-ef") || args[0].ToLower().Equals("-df"))) printUsage();
         }
 
@@ -83,26 +113,68 @@ namespace AESCrypto2021
         {
             Console.WriteLine(
               "\n" +
-              "Usage: AESCrypto2021 -ec <password> <input>\n" +
-              "   or: AESCrypto2021 -dc <password> <input>\n" +
-              "   or: AESCrypto2021 -ef <password> <input file> <output file>\n" +
-              "   or: AESCrypto2021 -df <password> <input file> <output file>\n" +
+              "AESCrypto2021 - Copyright (c) 2022 Peter Vils Hansen <peter.vils.hansen@gmail.com>\n" +
+              "\n" +
+              "Easily encrypt data using AES-GCM with 256 bit key size. Password hashed using PBKDF2-HMAC-SHA-256 with 200,001 rounds.\n" +
+              "\n" +
+              "Usage: AESCrypto2021 -ec\n" +
+              "   or: AESCrypto2021 -dc\n" +
+              "   or: AESCrypto2021 -ef <input file> <output file>\n" +
+              "   or: AESCrypto2021 -df <input file> <output file>\n" +
               "Examples:\n" +
-              "    Encrypt input from console with password 'foo' (end input with Ctrl+Z, Enter): AESCrypto2021 -ec foo\n" +
-              "    Decrypt input from console with password 'foo' (end input with Ctrl+Z, Enter): AESCrypto2021 -dc foo\n" +
-              "    Encrypt input from console piped in with password 'bar': echo Hello World | AESCrypto2021 -ec bar\n" +
-              "    Decrypt input from console piped in with password 'bar': type secret.json | AESCrypto2021 -dc bar\n" +
-              "    Encrypt file 'secret.txt' with password 'jazz': AESCrypto2021 -ef jazz secret.txt encrypted-secret.json\n" +
-              "    Decrypt file 'encrypted-secret.json' with password 'jazz': AESCrypto2021 -df jazz encrypted-secret.json secret.txt"
+              "    Encrypt input from console: AESCrypto2021 -ec\n" +
+              "    Decrypt input from console: AESCrypto2021 -dc\n" +
+              "    Encrypt file 'secret.txt': AESCrypto2021 -ef secret.txt secret-output.txt\n" +
+              "    Decrypt file 'encrypted-secret.json': AESCrypto2021 -df secret-output.txt decrypted.txt\n" + 
+              "\n" + 
+              "    A secure password will be generated automatically. It is not possible to input your own password."
               );
             Environment.Exit(0);
         }
 
+        static string readPassphraseFromConsole()
+        {
+            string passPhrase = "";
+            Console.Write("Input your pass phrase - finish with Enter: ");
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    passPhrase += key.KeyChar;
+                    Console.Write("*");
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            } while (true);
+
+            if (string.IsNullOrEmpty(passPhrase))
+            {
+                throw new ArgumentException("Empty pass phrase -- aborting...");
+            }
+
+            return passPhrase;
+        }
+
         static string readInputFromConsole()
         {
+            Console.WriteLine("Input your message - finish with an empty line (= Enter twice)...");
             string line;
             string input = "";
-            while ((line = Console.ReadLine()) != null) { input += line + "\n"; }
+            do
+            {
+                line = Console.ReadLine();
+                input += line;
+            } while (line != null && line.Length > 0);
+
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException("Empty input -- aborting...");
+            }
+
             return input;
         }
     }
